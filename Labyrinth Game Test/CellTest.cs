409d3ea4462acb9c2c5a9c4bbf3b6bc63ae83884
Desislava Yordanova <dess.yordanova@gmail.com>
2013-05-08 16:14:42 +0300

    Add Unit Testing project and organize 2 folders for the projects

---
+++ Labyrinth Game Test/CellTest.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Labyrinth;
+
+namespace Labyrinth_Game_Test
+{
+    [TestClass]
+    public class CellTest
+    {
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void CellWithNegativeRow()
+        {
+            Cell cell = new Cell(-1, 0);
+        }
+
+        [TestMethod]
+        public void CellIsEmptyTest()
+        {
+            Cell cell = new Cell(0, 0);
+            bool actual = cell.IsEmpty();
+
+            Assert.IsTrue(actual);
+        }
+
+        [TestMethod]
+        public void CellIsNotEmptyTest()
+        {
+            Cell cell = new Cell(0, 0, CellType.Wall);
+            bool actual = cell.IsEmpty();
+
+            Assert.IsFalse(actual);
+        }
+    }
+}
