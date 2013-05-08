620f640e5fb7639c4c7556512f6d92aa52948318
Desislava Yordanova <dess.yordanova@gmail.com>
2013-05-08 16:36:11 +0300

    10 unit tests for class cell added

--- Labyrinth Game Test/CellTest.cs
+++ Labyrinth Game Test/CellTest.cs
@@ -15,6 +15,13 @@ namespace Labyrinth_Game_Test
         }
 
         [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void CellWithNegativeCol()
+        {
+            Cell cell = new Cell(0, -1);
+        }
+
+        [TestMethod]
         public void CellIsEmptyTest()
         {
             Cell cell = new Cell(0, 0);
@@ -30,6 +37,60 @@ namespace Labyrinth_Game_Test
             bool actual = cell.IsEmpty();
 
             Assert.IsFalse(actual);
+        }
+
+        [TestMethod]
+        public void CellSetTypeTest()
+        {
+            Cell cell = new Cell(0, 0);
+            cell.Type = CellType.Wall;
+
+            Assert.AreEqual(CellType.Wall,cell.Type);
+        }
+
+        [TestMethod]
+        public void CellSetRowTest()
+        {
+            Cell cell = new Cell(0, 0);
+            cell.Row = 5;
+
+            Assert.AreEqual(5, cell.Row);
+        }
+
+        [TestMethod]
+        public void CellSetColTest()
+        {
+            Cell cell = new Cell(0, 0);
+            cell.Col = 5;
+
+            Assert.AreEqual(5, cell.Col);
+        }
+
+        [TestMethod]
+        public void CellWallToStringTest()
+        {
+            Cell cell = new Cell(0, 0, CellType.Wall);
+            string actual = cell.ToString();
+
+            Assert.AreEqual("#", actual);
+        }
+
+        [TestMethod]
+        public void CellPlayerToStringTest()
+        {
+            Cell cell = new Cell(0, 0, CellType.Player);
+            string actual = cell.ToString();
+
+            Assert.AreEqual("*", actual);
+        }
+
+        [TestMethod]
+        public void CellEmptyToStringTest()
+        {
+            Cell cell = new Cell(0, 0);
+            string actual = cell.ToString();
+
+            Assert.AreEqual("-", actual);
         }
     }
 }
