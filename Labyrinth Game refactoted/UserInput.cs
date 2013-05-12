ed9c4b0505bee5c3f50cdf84a7edd9f4d624b46b
Milena Bozhanova <milena.bozhanova@gmail.com>
2013-05-12 03:14:09 -0600

    Update UserInput.cs
    method UserInput - deleted
    
    implement IUserInterface and add method ProcessInput

--- Labyrinth Game refactoted/UserInput.cs
+++ Labyrinth Game refactoted/UserInput.cs
@@ -3,16 +3,90 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 
-namespace Labyrinth
+namespace Labyrinth 
 {
-    public static class UserInput
-    {
-        public static string GetInput()
-        {
-            Console.Write(MenuMessages.EnterMove);
-            string inputLine = Console.ReadLine();
-            return inputLine;
+    public static class UserInput : IUserInterface
+    {       
+        public void ProcessInput()
+        {            
+            if (Console.KeyAvailable)
+            {
+                var keyInfo = Console.ReadKey();
+                if (keyInfo.Key.Equals(ConsoleKey.LeftArrow))
+                {
+                    if (this.OnLeftPressed != null)
+                    {
+                        this.OnLeftPressed(this, new EventArgs());
+                    }
+                }
+
+                else if (keyInfo.Key.Equals(ConsoleKey.RightArrow))
+                {
+                    if (this.OnRightPressed != null)
+                    {
+                        this.OnRightPressed(this, new EventArgs());
+                    }
+                }
+                
+                else if (keyInfo.Key.Equals(ConsoleKey.UpArrow))
+                {
+                    if (this.OnRightPressed != null)
+                    {
+                        this.OnUpPressed(this, new EventArgs());
+                    }
+                }
+                
+                else if (keyInfo.Key.Equals(ConsoleKey.DownArrow))
+                {
+                    if (this.OnRightPressed != null)
+                    {
+                        this.OnDownPressed(this, new EventArgs());
+                    }
+                }
+
+                else if (keyInfo.Key.Equals(ConsoleKey.R))
+                {
+                    if (this.OnRestartPressed != null)
+                    {
+                        this.OnRestartPressed(this, new EventArgs());
+                    }
+                }
+                
+                else if (keyInfo.Key.Equals(ConsoleKey.T))
+                {
+                    if (this.OnTopScorePressed != null)
+                    {
+                        this.OnTopScorePressed(this, new EventArgs());
+                    }
+                }
+                
+                else if (keyInfo.Key.Equals(ConsoleKey.E))
+                {
+                    if (this.OnExitPressed; != null)
+                    {
+                        this.OnExitPressed;(this, new EventArgs());
+                    }
+                }
+                
+                else 
+                {
+                    Console.WriteLine(MenuMessages.InvalidCommand);
+                }
+            }
         }
 
+        public event EventHandler OnLeftPressed;
+
+        public event EventHandler OnRightPressed;
+
+        public event  EventHandler OnUpPressed;
+
+        public event EventHandler OnDownPressed;
+
+        public event EventHandler OnRestartPressed;
+
+        public event EventHandler OnExitPressed;
+
+        public event EventHandler OnTopScorePressed;
     }
 }
