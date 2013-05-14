7c2f0216d339345fa5508e612ad4dd18cfddcb64
Stiliyan Vasilev <stiliyan.vasilev@gmail.com>
2013-05-14 19:52:22 +0300

    Rename Unit Tests
    - Cell.Tests;
    - Playfield.Tests;
    - ScoreBoard.Tests;

---
+++ Labyrinth Game Test/Cell.Tests.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Labyrinth;
+
+namespace Labyrinth_Game_Test
+{
+    [TestClass]
+    public class CellTest
+    {
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void CellWithNegativeRow()
+        {
+            Cell cell = new Cell(-1, 0);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void CellWithNegativeCol()
+        {
+            Cell cell = new Cell(0, -1);
+        }
+
+        [TestMethod]
+        public void CellIsEmptyTest()
+        {
+            Cell cell = new Cell(0, 0);
+            bool actual = cell.IsEmpty();
+
+            Assert.IsTrue(actual);
+        }
+
+        [TestMethod]
+        public void CellIsNotEmptyTest()
+        {
+            Cell cell = new Cell(0, 0, CellType.Wall);
+            bool actual = cell.IsEmpty();
+
+            Assert.IsFalse(actual);
+        }
+
+        [TestMethod]
+        public void CellSetTypeTest()
+        {
+            Cell cell = new Cell(0, 0);
+            cell.Type = CellType.Wall;
+
+            Assert.AreEqual(CellType.Wall,cell.Type);
+        }
+
+        [TestMethod]
+        public void CellSetRowTest()
+        {
+            Cell cell = new Cell(0, 0);
+            cell.Row = 5;
+
+            Assert.AreEqual(5, cell.Row);
+        }
+
+        [TestMethod]
+        public void CellSetColTest()
+        {
+            Cell cell = new Cell(0, 0);
+            cell.Col = 5;
+
+            Assert.AreEqual(5, cell.Col);
+        }
+
+        [TestMethod]
+        public void CellWallToStringTest()
+        {
+            Cell cell = new Cell(0, 0, CellType.Wall);
+            string actual = cell.ToString();
+
+            Assert.AreEqual("#", actual);
+        }
+
+        [TestMethod]
+        public void CellPlayerToStringTest()
+        {
+            Cell cell = new Cell(0, 0, CellType.Player);
+            string actual = cell.ToString();
+
+            Assert.AreEqual("*", actual);
+        }
+
+        [TestMethod]
+        public void CellEmptyToStringTest()
+        {
+            Cell cell = new Cell(0, 0);
+            string actual = cell.ToString();
+
+            Assert.AreEqual("-", actual);
+        }
+    }
+}
---
+++ Labyrinth Game Test/ScoreBoard.Tests.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Labyrinth;
+using System.Collections.Generic;
+
+namespace Labyrinth_Game_Test
+{
+    [TestClass]
+    public class ScoreBoardTest
+    {
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void ScoreBoardSetNullResultsTest()
+        {
+            ScoreBoard scoreboard = new ScoreBoard(5);
+            scoreboard.TopResults = null;
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void ScoreBoardExceedCapacityResultsTest()
+        {
+            ScoreBoard scoreboard = new ScoreBoard(5);
+            Result result = null;
+            List<Result> results = new List<Result>();
+
+            for (int i = 0; i < scoreboard.TopResults.Capacity + 1; i++)
+            {
+                result = new Result(i, "Player" + i);
+                results.Add(result);
+            }
+
+            scoreboard.TopResults = results;
+        }
+
+        [TestMethod]
+        public void ScoreBoardSetResultsTest()
+        {
+            ScoreBoard scoreboard = new ScoreBoard(5);
+            Result result = null;
+            List<Result> results = new List<Result>();
+
+            for (int i = 0; i < scoreboard.TopResults.Capacity; i++)
+            {
+                result = new Result(i, "Player" + i);
+                results.Add(result);
+            }
+
+            scoreboard.TopResults = results;
+        }
+
+        [TestMethod]
+        public void ScoreBoardAddResultsTest()
+        {
+            ScoreBoard scoreboard = new ScoreBoard(5);
+            scoreboard.AddResult(5, "Player" + 5);
+            scoreboard.AddResult(6, "Player" + 6);
+            scoreboard.AddResult(2, "Player" + 2);
+            scoreboard.AddResult(4, "Player" + 4);
+            scoreboard.AddResult(7, "Player" + 7);
+            scoreboard.AddResult(3, "Player" + 3);
+            int actual = scoreboard.TopResults[1].MovesCount;
+
+            Assert.AreEqual(3, actual);
+        }
+
+        [TestMethod]
+        public void ScoreBoardNotAddedResultsTest()
+        {
+            ScoreBoard scoreboard = new ScoreBoard(5);
+            scoreboard.AddResult(5, "Player" + 5);
+            scoreboard.AddResult(6, "Player" + 6);
+            scoreboard.AddResult(2, "Player" + 2);
+            scoreboard.AddResult(4, "Player" + 4);
+            scoreboard.AddResult(7, "Player" + 7);
+            scoreboard.AddResult(9, "Player" + 9);
+            int actual = scoreboard.TopResults[4].MovesCount;
+
+            Assert.AreEqual(7, actual);
+        }
+
+        [TestMethod]
+        public void ScoreBoardEmptyToStringTest()
+        {
+            ScoreBoard scoreboard = new ScoreBoard(5);
+            string actual = scoreboard.ToString();
+
+            Assert.AreEqual("\nThe scoreboard is empty!", actual);
+        }
+
+        [TestMethod]
+        public void ScoreBoardToStringTest()
+        {
+            ScoreBoard scoreboard = new ScoreBoard(5);
+            scoreboard.AddResult(5, "Player" + 5);
+            scoreboard.AddResult(6, "Player" + 6);
+            scoreboard.AddResult(2, "Player" + 2);
+            scoreboard.AddResult(4, "Player" + 4);
+            scoreboard.AddResult(7, "Player" + 7);
+            string actual = scoreboard.ToString();
+            string expected = "\n1. Player2 --> 2 moves\n" +
+                              "2. Player4 --> 4 moves\n" +
+                              "3. Player5 --> 5 moves\n" +
+                              "4. Player6 --> 6 moves\n" +
+                              "5. Player7 --> 7 moves\n";
+
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
